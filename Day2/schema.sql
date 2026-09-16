70f52d7eee84d95fd702dc5e5196db0d5c4503ea
Gauresh P A <gauresh.pa@vonnue.com>
2026-09-16 15:03:24 +0530

    chore: added DDL queries from Day 1

--- Day2/schema.sql
+++ Day2/schema.sql
@@ -1,3 +1,87 @@
+-- 1. USERS
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+-- 2. CUSTOMERS
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+-- 3. CATEGORIES
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+-- 3. TICKETS
+
+CREATE TABLE tickets (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    status VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+
+-- 4. COMMENTS
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL;
+    ticket_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (ticket_id) REFERENCES tickets(id)
+);
+
+
+-- 5. ASSIGNMENTS
+
+CREATE TABLE assignments (
+    id SERIAL PRIMARY KEY,
+    ticket_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    assigned_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    unassigned_at TIMESTAMP,
+
+    FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+-- 6. STATUS HISTORY
+
+CREATE TABLE status_history (
+    id SERIAL PRIMARY KEY,
+    ticket_id INTEGER NOT NULL,
+    status VARCHAR(30) NOT NULL CHECK (status IN ('open', 'in-progress', 'resolved', 'closed')),
+    changed_by INTEGER NOT NULL,
+    changed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+    FOREIGN KEY (changed_by) REFERENCES users(id)
+);
+
+
 -- Add relation between COMMENTS and USER table
 ALTER TABLE comments
 ADD COLUMN user_id INTEGER NOT NULL;
